b0c53fd49869a875d9eb91f3606df6b5b79b2a53
Coleman Buffa <coleman.buffa@gmail.com>
2021-05-13 09:35:17 -0700

    Initialize project directory.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+    top banner
+    section -intro -portfolio -about -contact
+    </div>
+  );
+}
+
+export default App;
