4171d5953de07632b4776ce335ad6f81a9b6ea41
Coleman Buffa <coleman.buffa@gmail.com>
2021-05-20 18:48:03 -0700

    Consume project API data in works component.

--- client/src/components/works/works.jsx
+++ client/src/components/works/works.jsx
@@ -1,38 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './works.scss';
+import API from '../../utils/API';
 
 export default function Works() {
 
   const [currentSlide, setCurrentSlide] = useState(0);
-  const data = [
-    {
-      id: "1",
-      icon: "./assets/mobile.png",
-      title: "Web Design",
-      desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-      img:
-        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
-    },
-    {
-      id: "2",
-      icon: "./assets/globe.png",
-      title: "Mobile Application",
-      desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      img:
-        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
-    },
-    {
-      id: "3",
-      icon: "./assets/writing.png",
-      title: "Branding",
-      desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      img:
-        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
-    },
-  ];
+  const [data, setData] = useState([]);
+  // const data = [
+  //   {
+  //     id: "1",
+  //     icon: "./assets/mobile.png",
+  //     title: "Web Design",
+  //     desc:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+  //     img:
+  //       "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+  //   },
+  //   {
+  //     id: "2",
+  //     icon: "./assets/globe.png",
+  //     title: "Mobile Application",
+  //     desc:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  //     img:
+  //       "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+  //   },
+  //   {
+  //     id: "3",
+  //     icon: "./assets/writing.png",
+  //     title: "Branding",
+  //     desc:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  //     img:
+  //       "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+  //   },
+  // ];
+
+  useEffect (() => {
+    loadProjects();
+  }, []);
+
+  const loadProjects = () => {
+    API.getProjects()
+    .then(result => {
+      setData(result.data);
+    })
+  }
 
   const handleClick = (direction) => {
     if (direction === "left") {
@@ -56,7 +69,10 @@ export default function Works() {
     <div className="works" id="works">
       <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
         {data.map((d) => (
-          <div className="container">
+          
+          <div className="container"
+          key={d._id}
+          >
             <div className="item">
               <div className="left">
                 <div className="leftContainer">
@@ -64,12 +80,11 @@ export default function Works() {
                     <img src={d.icon} alt="" />
                   </div>
                   <h2>{d.title}</h2>
-                  <p>{d.desc}</p>
-                  <span>Projects</span>
+                  <p>{d.description}</p>
                 </div>
               </div>
               <div className="right">
-                <img src="http://placeimg.com/640/480/tech" alt="" />
+                <img src={d.image_link} alt="" />
               </div>
             </div>
           </div>
