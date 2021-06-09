09a91a0adc2cf7379e4305fddde815b3e75ea118
Coleman Buffa <coleman.buffa@gmail.com>
2021-06-09 16:40:52 -0700

    Draft and style about me cards.

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -1,11 +1,127 @@
 import React, { useState } from 'react';
 import './about.scss';
+import { Grid, Paper, Typography } from '@material-ui/core';
 
 export default function About() {
 
   return (
-    <div className="about" id='about'>
-      About!
-      </div>
+    <section className="about" id='about'>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={4}>
+          <Paper variant="outlines" elevation={3} className="aboutWrapper">
+            <br />
+            <Typography variant="h6">
+              Software Engineer
+          </Typography>
+            <br />
+            <Typography variant="caption" >
+              Building practical and secure applications <br />
+              to meet your business needs.
+          </Typography>
+            <br />
+            <Typography variant="h6">
+              Technologies I use
+            </Typography>
+            <br />
+            <Typography variant="caption">
+              Node.js, Express.js, <br />
+              MySQL, MongoDB, DynamoDB, S3 <br />
+
+            </Typography>
+            <br />
+            <Typography variant="h6">
+              Dev Tools
+            </Typography>
+            <br />
+            <Typography variant="caption">
+              Sequelize and Mongoose <br />
+              Database Architechting <br />
+              Application Design <br />
+              Secure Authentication
+          </Typography>
+            <br />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper variant="outlines" elevation={3} className="aboutWrapper">
+            <br />
+            <Typography variant="h6">
+              Web Developer
+          </Typography>
+            <br />
+            <Typography variant="caption">
+              I like to build big with a <br />
+              healthy dose of collaboration.
+          </Typography>
+            <br />
+            <Typography variant="h6">
+              Languages I speak
+            </Typography>
+            <br />
+
+            <Typography variant="caption">
+              HTML5, CSS3, Sass <br />
+              JavaScript, React <br />
+            </Typography>
+            <br />
+
+            <Typography variant="h6">
+              Dev Tools
+            </Typography>
+            <br />
+
+            <Typography variant="caption">
+              Material Desgin <br />
+              Bootstrap <br />
+              GitHub <br />
+              Terminal <br />
+              VSCode <br />
+              Auth0
+          </Typography>
+            <br />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper variant="outlines" elevation={3} className="aboutWrapper">
+            <br />
+            <Typography variant="h6">
+              Geology and Woodwork
+          </Typography>
+            <br />
+            <Typography variant="caption">
+              Earth Scientist and natural <br />
+              historian who appreciates <br />
+              the natural world.
+          </Typography>
+            <br />
+            <Typography variant="h6">
+              Experience
+            </Typography>
+            <br />
+
+            <Typography variant="caption">
+              Environmental Consulting <br />
+              Professional Luthier <br />
+            </Typography>
+            <br />
+
+            <Typography variant="h6">
+              Skills
+            </Typography>
+            <br />
+
+            <Typography variant="caption">
+              Systematic problem solver <br />
+              Utilize complex toolset <br />
+              Constant learner
+
+
+            </Typography>
+            <br />
+          </Paper>
+        </Grid>
+
+      </Grid>
+    </section>
   )
 }
